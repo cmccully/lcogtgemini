Bugfix in normalizing flats.

lcogtgemini/flats.py
@@ -90,7 +90,7 @@ def makemasterflat(flatfiles, rawpath, plot=True):
                                          :unmosaiced_hdu[i].data.shape[1]]
                 # If more than half of the wavelengths in this amp are on the chip
                 midline = wavelengths_hdu[i].data.shape[0] // 2
-                in_chip = np.logical_and(unmosaiced_wavelengths[midline] >= chip[0], unmosaiced_wavelengths[midline] <= chip[1])
+                in_chip = np.logical_and(unmosaiced_wavelengths[midline] >= min(chip), unmosaiced_wavelengths[midline] <= max(chip))
                 if in_chip.sum() > 0.5 * in_chip.shape[0]:
                     unmosaiced_hdu[i].data /= fitting.eval_fit(best_fit, unmosaiced_wavelengths)
 

lcogtgemini/flux_calibration.py
@@ -90,7 +90,7 @@ def specsens(specfile, outfile, stdfile, wavelengths_filename, exptime=None,
         in_chip = np.logical_and(observed_wavelengths >= min(chip), observed_wavelengths <= max(chip))
         best_fit, n_poly, n_fourier = fit_sensitivity(observed_wavelengths[in_chip], observed_data[in_chip],
                                                       telluric_model['col1'], telluric_model['col2'], standard['col1'], standard['col2'],
-                                                      11, 0, float(observed_hdu['SCI'].header['RDNOISE']), good_pixels[in_chip])
+                                                      3, 11, float(observed_hdu['SCI'].header['RDNOISE']), good_pixels[in_chip])
 
         # Strip out the telluric correction
         best_fit['popt'] = best_fit['popt'][6:]